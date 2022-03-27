69ab1c51ee62feb8b803e5a484a8b74d4026e7ee
phamhung2312 <90098016+phamhung2312@users.noreply.github.com>
2022-03-27 21:45:54 +0700

    Add files via upload

---
+++ RandomInt2D.java
@@ -0,0 +1,99 @@
+/*************************************************
+ * 
+ * Execution: java RandomInt N k
+ * 
+ * Prints k pseudo-random integers between 0 and N-1.
+ * 
+ * @version 1.0
+ *
+ **************************************************/
+public class RandomInt2D {
+
+    /**
+     * Print some pseudo-random integers in a given interval
+     * 
+     * @param args[0] determines the interval of each pseudo-random integer
+     * @param args[1] gives the number of generated pseudo-random integers
+     */
+    public static void main(String[] args) {
+        // a positive integer
+        int n = Integer.parseInt(args[1]);
+        int k = Integer.parseInt(args[0]);
+        // int ss0 = 0;
+        // int ss1 = 0;
+        // int ss2 = 0;
+        // int[] count0 = new int[500];
+        // int[] count1 = new int[500];
+        // int[] count2 = new int[500];
+        int[][] count = new int[n][500];
+        int[] ssn = new int[n];
+        for (int j = 0; j < 500; j++) {
+            for (int a = 0; a < n; a++) {
+                ssn[a] = 0;
+            }
+
+            // ss0 = 0;
+            // ss1 = 0;
+            // ss2 = 0;
+            for (int i = 0; i < k; i++) {
+                ssn[random(n)] += 1;
+
+                // switch (random(n)) {
+                // case 0:
+                // ss0++;
+                // break;
+                // case 1:
+                // ss1++;
+                // break;
+                // case 2:
+                // ss2++;
+                // break;
+
+                // }
+            }
+            for (int m = 0; m < n; m++) {
+                count[m][j] = ssn[m];
+            }
+            // count[0][j] = ss0;
+            // count[1][j] = ss1;
+            // count[2][j] = ss2;
+        }
+        // System.out.println("so so 0 la:" + ss0);
+        // System.out.println("so so 1 la:" + ss1);
+        // System.out.println("so so 2 la:" + ss2);
+        for (int i = 0; i < n; i++) {
+            System.out.println("count" + i + " la: " + meanArray(count[i]));
+        }
+        // System.out.println("count0 la: " + meanArray(count[0]));
+        // System.out.println("count1 la: " + meanArray(count[1]));
+        // System.out.println("count2 la: " + meanArray(count[2]));
+    }
+
+    /**
+     * A function returning a pseudo-random integer
+     * 
+     * @param n linh tinh
+     * @return linh tinh
+     */
+    public static int random(int n) {
+        // a pseudo-random real between 0.0 and 1.0
+        double r = Math.random();
+        return (int) (r * n);
+    }
+
+    public static double meanArray(int mean[]) {
+        double sum1 = 0;
+        int n = mean.length;
+        for (int i = 0; i < n; i++) {
+            sum1 += mean[i];
+        }
+        return sum1 / n;
+
+    }
+
+    /*
+     * We can choose to write directly :
+     * return (int) (Math.random() * n);
+     */
+
+}
